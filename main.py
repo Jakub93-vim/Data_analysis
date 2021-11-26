0a3847ed5e7ab178a424e7e88a7829e6f1e14ea9
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-11-26 17:42:54 +0100

    download csv

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import pandas as pd
 
+#http://www2.informatik.uni-freiburg.de/~cziegler/BX/BX-CSV-Dump.zip
+
+url="http://www2.informatik.uni-freiburg.de/~cziegler/BX/BX-CSV-Dump.zip"
+c=pd.read_csv(url, 'BX-Books.csv')
