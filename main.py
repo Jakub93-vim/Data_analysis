5d7483003a9f990aa3f5e470ec67b2469935d278
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-11-27 13:18:18 +0100

    dataset complete, cleaning data

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-#http://www2.informatik.uni-freiburg.de/~cziegler/BX/BX-CSV-Dump.zip
-
 from io import BytesIO
 import zipfile
 import requests
 import pandas as pd
 
-#file = 'BX-Books.csv'
 
 url = 'http://www2.informatik.uni-freiburg.de/~cziegler/BX/BX-CSV-Dump.zip'
 
@@ -15,13 +12,26 @@ request = requests.get(url)
 zipDocument = zipfile.ZipFile(BytesIO(request.content))
 zipDocument.extractall()
 '''
-
+#read all data
 books = pd.read_csv('BX-Books.csv', encoding='cp1251', sep=';', error_bad_lines=False)
 ratings = pd.read_csv('BX-Book-Ratings.csv', encoding='cp1251', sep=';')
 users = pd.read_csv('BX-Users.csv', encoding='cp1251', sep=';')
 
+#merge all data
 dataset_books_ratings = pd.merge(books,ratings, on= ['ISBN'])
 dataset_complete = pd.merge(dataset_books_ratings, users, on = ['User-ID'])
 
+#removes useless columns
+dataset_complete = dataset_complete.drop(columns=['Image-URL-S','Image-URL-L'])
+
+#making all data lower case
+dataset_complete = dataset_complete.applymap(lambda x: x.lower() if type(x) == str else x)
+
+#rearranging the dataset
+cols = list(dataset_complete.columns.values)
+#dataset_complete = dataset_complete[ cols[0:2] + cols[-4:-1] + cols[3:8] ]
+
+
+pd.set_option('display.width', 400)
 pd.set_option('max_columns', None)
 print(dataset_complete)
